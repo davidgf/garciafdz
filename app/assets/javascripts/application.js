6be77922c17fc70d648dc9e45a9b057d982729b5
David García <davidgf1987@gmail.com>
2015-02-11 00:00:27 +0100

    Header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,30 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+    var navbar = $('.navbar');
+    var navHeight = navbar.height();
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() >= navHeight) {
+            navbar.addClass('navbar-color');
+        }
+        else {
+            navbar.removeClass('navbar-color');
+        }
+    });
+
+    if($(window).width() <= 767) {
+        navbar.addClass('custom-collapse');
+    }
+
+    $(window).resize(function() {
+        if($(this).width() <= 767) {
+            navbar.addClass('custom-collapse');
+        }
+        else {
+            navbar.removeClass('custom-collapse');
+        }
+    });
+});
