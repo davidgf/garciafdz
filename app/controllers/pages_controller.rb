ac688a693ed2d1a3d1c2907aca43f9e6087723bd
David García <davidgf1987@gmail.com>
2015-02-16 12:23:25 +0100

    Contact mailer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def contact
+    ContactMailer.info_mail(params[:message]).deliver
+    respond_to do |format|
+      format.html {render layout: false}
+      format.json {render json: '{"ok": true}'}
+    end
+  end
 end
